74bfd51868d9010cbc4727e6f1cc894f9c4dfc53
han1683 <imranhani61@gmail.com>
2025-11-22 18:42:53 -0500

    Add docstring to get_connection function
    Added a docstring to the get_connection function.

--- Catalog/dp.py
+++ Catalog/dp.py
@@ -1,9 +1,13 @@
 import mysql.connector
 
 def get_connection():
+    """
+    Creates and returns a connection to the fresh_footwear MySQL database.
+    Make sure the 'fresh_footwear' DB and its tables are imported from your .sql files.
+    """
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",  # change if your MySQL has a password
+        password="",  # change this if your MySQL has a password
         database="fresh_footwear",
     )
